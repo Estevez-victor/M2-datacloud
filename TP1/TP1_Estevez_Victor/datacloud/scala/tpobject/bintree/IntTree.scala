69136b36fca32e8e8390837f9c5af303dcffc4cb
Estevez-victor <73241416+Estevez-victor@users.noreply.github.com>
2023-03-18 01:47:40 +0100

    Delete TP1 directory

--- TP1/TP1_Estevez_Victor/datacloud/scala/tpobject/bintree/IntTree.scala
+++
@@ -1,5 +0,0 @@
-package datacloud.scala.tpobject.bintree
-
-sealed abstract class IntTree
-case object EmptyIntTree extends IntTree
-case class NodeInt(elem : Int, left : IntTree, right : IntTree) extends IntTree
--- TP1/TP1_Estevez_Victor/datacloud/scala/tpobject/catalogue/Catalogue.scala
+++
@@ -1,9 +0,0 @@
-package datacloud.scala.tpobject.catalogue
-import scala.collection.Iterable
-
-trait Catalogue {
-  def getPrice(nom  : String) : Double
-  def removeProduct(nom : String) : Unit
-  def selectProducts(min: Double, max:Double) : Iterable[String]
-  def storeProduct(nom : String, prix: Double) : Unit
-}
